ec46c964e75ab4f9aff4c4ad3a2464a1ee75e2d7
Ritika Agrawal <agrawal.ritika201@gmail.com>
2025-11-24 06:15:21 -0700

    Added python code

---
+++ code.py
@@ -0,0 +1,14 @@
+print("Hello world")
+a= int(input("Enter first number:\n") )
+b= int(input("Enter Second number:\n") )
+
+print("the value of ", a ,"+",b, "is :", a+b)
+print("the value of ", a ,"-",b, "is :", a-b)
+print("the value of ", a ,"*",b, "is :", a*b)
+print("the value of ", a ,"/",b, "is :", a/b)
+print("the value of ", a ,"%",b, "is :", a%b)
+print("the value of ", a ,"//",b, "is :", a//b)
+print("the value of ", a ,"**",b, "is :", a**b)
+print("the type of a is :",type(a))
+print("the type of b is :",type(b))
+
